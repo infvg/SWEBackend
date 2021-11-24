63d503524f89b743f6bda04a673b3c8967b898d8
inf <ialhazmim@gmail.com>
2021-11-24 13:28:09 +0300

    Modified the reader to read competitions from multiple sheets

--- CompetitionReader/src/Reader.java
+++ CompetitionReader/src/Reader.java
@@ -15,45 +15,52 @@ public class Reader {
 		competitions = new LinkedList<>();
 		FileInputStream fis = new FileInputStream(new File("competitions.xlsx"));
 		XSSFWorkbook wb = new XSSFWorkbook(fis);
-		XSSFSheet sheet = wb.getSheetAt(0);
-		Team currentTeam = null;
-		Competition currentComp = null;
 		DataFormatter formatter = new DataFormatter();
+
 		int id = 0;
-		for (Row row : sheet) {
-			switch (row.getCell(0).getCellTypeEnum()) {
-			case STRING:
-				String c0 = row.getCell(0).getStringCellValue();
-				if (c0.equalsIgnoreCase("competition name")) {
-					if (currentComp != null)
-						competitions.add(currentComp);
-					currentComp = new Competition(row.getCell(1).getStringCellValue());
-				} else if (c0.equalsIgnoreCase("competition link")) {
-					currentComp.link = row.getCell(1).getStringCellValue();
-				} else if (c0.equalsIgnoreCase("competition date")) {
-					currentComp.date = row.getCell(1).getDateCellValue();
+		for (int i = 0; i < wb.getNumberOfSheets(); i++) {
+			XSSFSheet sheet = wb.getSheetAt(i);
+			Team currentTeam = null;
+			Competition currentComp = null;
+			for (Row row : sheet) {
+				switch (row.getCell(0).getCellTypeEnum()) {
+				case STRING:
+					String c0 = row.getCell(0).getStringCellValue();
+					if (c0.equalsIgnoreCase("competition name")) {
+						if (currentComp != null)
+							competitions.add(currentComp);
+						currentComp = new Competition(row.getCell(1).getStringCellValue());
+					} else if (c0.equalsIgnoreCase("competition link")) {
+						currentComp.link = row.getCell(1).getStringCellValue();
+					} else if (c0.equalsIgnoreCase("competition date")) {
+						currentComp.date = row.getCell(1).getDateCellValue();
+					}
+					break;
+				case BLANK:
+					if (currentTeam != null)
+						currentComp.teams.add(currentTeam);
+					currentTeam = new Team(id);
+					id++;
+					break;
+				case NUMERIC:
+					currentTeam.students.add(
+							new Student(row.getCell(2).getStringCellValue(), formatter.formatCellValue(row.getCell(1)),
+									row.getCell(3).getStringCellValue(), formatter.formatCellValue(row.getCell(4))));
+				default:
+					break;
 				}
-				break;
-			case BLANK:
-				if (currentTeam != null)
-					currentComp.teams.add(currentTeam);
-				currentTeam = new Team(id);
-				id++;
-				break;
-			case NUMERIC:
-				currentTeam.students
-						.add(new Student(row.getCell(2).getStringCellValue(), formatter.formatCellValue(row.getCell(1)),
-								row.getCell(3).getStringCellValue(), formatter.formatCellValue(row.getCell(4))));
-			default:
-				break;
 			}
+			if (currentTeam != null)
+				currentComp.teams.add(currentTeam);
+			competitions.add(currentComp);
 		}
-		if (currentTeam != null)
-			currentComp.teams.add(currentTeam);
-		System.out.println(String.format("%s %s %s", currentComp.name, currentComp.link, currentComp.date.toString()));
-		for (Team team : currentComp.teams) {
-			for (Student student : team.students) {
-				System.out.println(String.format("%s %s %s %s", student.name, student.id, student.major, student.rank));
+		for (Competition comp : competitions) {
+			System.out.println(String.format("%s %s %s", comp.name, comp.link, comp.date.toString()));
+			for (Team team : comp.teams) {
+				for (Student student : team.students) {
+					System.out.println(
+							String.format("%s %s %s %s", student.name, student.id, student.major, student.rank));
+				}
 			}
 		}
 		wb.close();
